75425c3322e26129f8243f02710348fb89403085
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-09-10 12:23:04 -0300

    Conexão com o mongodb

---
+++ src/index-mongo.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express, { Request, Response } from 'express';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI!)
+await client.connect()
+const db = client.db(process.env.MONGO_DB)
+const app = express();
+app.use(express.json());
+
+app.get('/estudantes', async (req: Request, res: Response) => {
+    const estudantes = await db.collection('estudantes').find().toArray();
+    res.status(200).json(estudantes);
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
