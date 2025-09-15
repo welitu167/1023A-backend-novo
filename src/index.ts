ac47afa6e04d2d0a3b71939a53efd348776e89e4
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-09-15 17:50:37 -0300

    Rollback

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,66 @@
 import 'dotenv/config'
+import mysql from 'mysql2/promise';
+console.log(process.env.DBUSER);
+
 import express, { Request, Response } from 'express';
-import { MongoClient } from 'mongodb';
-
-const client = new MongoClient(process.env.MONGODB_URI!)
-await client.connect()
-const db = client.db(process.env.MONGO_DB)
 const app = express();
-app.use(express.json());
 
-app.get('/estudantes', async (req: Request, res: Response) => {
-    const estudantes = await db.collection('estudantes').find().toArray();
-    res.status(200).json(estudantes);
+app.get('/', async (req: Request, res: Response) => {
+    if (!process.env.DBUSER) {//! significa que é a negação da variável
+        res.status(500).send("Variável de ambiente DBUSER não está definida")
+        return;
+    }
+    if (process.env.DBPASSWORD==undefined) {
+        res.status(500).send("Variável de ambiente DBPASSWORD não está definida")
+        return;
+    }
+    if (!process.env.DBHOST) {
+        res.status(500).send("Variável de ambiente DBHOST não está definida")
+        return;
+    }
+    if (!process.env.DBNAME) {
+        res.status(500).send("Variável de ambiente DBNAME não está definida")
+        return;
+    }
+    if (!process.env.DBPORT) {
+        res.status(500).send("Variável de ambiente DBPORT não está definida")
+        return;
+    }
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DBHOST,
+            user: process.env.DBUSER,
+            password: process.env.DBPASSWORD,
+            database: process.env.DBNAME,
+            port: Number(process.env.DBPORT)
+        })
+        res.send("Conectado ao banco de dados com sucesso!");
+        await connection.end();
+    }
+    catch (error) {
+        res.status(500).send("Erro ao conectar ao banco de dados: " + error);
+    }
 });
+
+
+//Tarefa: Criar uma rota get para produtos que retorne a lista de produtos do banco de dados
+//O produto deve ter id, nome preco, urlfoto, descricao
+//Deve-se criar uma tabela no banco de dados AIVEN para armazenar os produtos
+//A resposta deve ser um array de produtos em formato JSON
+//Crie o código sql para criar a tabela de produtos
+/* 
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    urlfoto VARCHAR(255) NOT NULL,
+    descricao TEXT
+);
+Faz pelo menos 3 inserções nessa tabela
+*/ 
+
+
+
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
