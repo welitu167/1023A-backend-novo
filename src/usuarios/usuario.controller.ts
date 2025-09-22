f2574c439835ce5becc7c4724263aa3121e69bdc
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-09-22 10:40:00 -0300

    Atualização do código

---
+++ src/usuarios/usuario.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+class UsuarioController {
+    adicionar(req:Request, res:Response) {
+    }
+    listar(req:Request, res:Response) {
+
+    }
+}
+export default new UsuarioController();
